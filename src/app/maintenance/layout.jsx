f9f684aa192efefef24ebab1f7660d8cc3c6c669
roteroam <140432727+realphilamator@users.noreply.github.com>
2026-03-23 20:58:42 -0500

    yg

--- src/app/maintenance/layout.jsx
+++ src/app/maintenance/layout.jsx
@@ -6,6 +6,8 @@ export const metadata = {
 export default function MaintenanceLayout({ children }) {
   return (
     <html lang="en">
+      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5527734260417462"
+          crossorigin="anonymous"></script></head>
       <body>
         {children}
       </body>
